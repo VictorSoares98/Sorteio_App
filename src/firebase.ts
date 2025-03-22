2c65835e2c1f52763fe1ec5b86b0300a92a9aa78
Victor Stark <147451621+VictorSoares98@users.noreply.github.com>
2025-03-22 15:59:54 -0300

    atualiza configurações do Firebase com credenciais do projeto

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,14 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // Suas credenciais do Firebase aqui
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_ID",
-  appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyDZvWlQfdskmFmyiRHxjldX6VsY_2JlnRw",
+    authDomain: "app-sorteio-2401e.firebaseapp.com",
+    projectId: "app-sorteio-2401e",
+    storageBucket: "app-sorteio-2401e.firebasestorage.app",
+    messagingSenderId: "346696645288",
+    appId: "1:346696645288:web:3bf3a92f8ea606f4bf609d"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
