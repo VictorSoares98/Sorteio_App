a0adbe0fd5b15ebca5de8853d53e6eebf8370060
Victor Stark <147451621+VictorSoares98@users.noreply.github.com>
2025-03-26 02:15:27 -0300

    corrige URL do bucket de armazenamento no Firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDZvWlQfdskmFmyiRHxjldX6VsY_2JlnRw",
     authDomain: "app-sorteio-2401e.firebaseapp.com",
     projectId: "app-sorteio-2401e",
-    storageBucket: "app-sorteio-2401e.appspot.com", // Corrigido: formato correto para o Storage
+    storageBucket: "app-sorteio-2401e.firebasestorage.app",
     messagingSenderId: "346696645288",
     appId: "1:346696645288:web:3bf3a92f8ea606f4bf609d"
   };
