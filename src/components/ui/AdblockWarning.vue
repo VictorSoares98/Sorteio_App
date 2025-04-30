d00b53d0f6e1c14a3b893ef4998eeb09152c58ee
Victor Stark <147451621+VictorSoares98@users.noreply.github.com>
2025-04-30 00:18:14 -0300

    feat: aprimora mensagem de aviso de conectividade e ajusta estilo

--- src/components/ui/AdblockWarning.vue
+++ src/components/ui/AdblockWarning.vue
@@ -27,7 +27,9 @@ defineExpose({ dismiss });
   <transition name="fade">
     <div 
       v-if="isVisible" 
-      class="fixed bottom-4 right-4 z-50 p-4 bg-yellow-50 border border-yellow-200 rounded-lg shadow-lg max-w-md"
+      class="fixed z-50 p-3 sm:p-4 border border-yellow-200 rounded-lg shadow-lg bg-yellow-50 
+             bottom-2 right-2 sm:bottom-4 sm:right-4 
+             w-[calc(100%-16px)] sm:w-auto sm:max-w-md"
     >
       <div class="flex items-start">
         <div class="flex-shrink-0 mt-0.5">
@@ -35,10 +37,10 @@ defineExpose({ dismiss });
             <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
           </svg>
         </div>
-        <div class="ml-3">
-          <h3 class="text-sm font-medium text-yellow-800">Bloqueio de Firestore detectado</h3>
+        <div class="ml-3 flex-grow">
+          <h3 class="text-sm font-medium text-yellow-800">Problema de conectividade detectado</h3>
           <div class="mt-1 text-xs text-yellow-700">
-            <p>Um bloqueador de anúncios ou extensão está impedindo a conexão com o Firestore, afetando a sincronização em tempo real.</p>
+            <p>Um bloqueador de anúncios ou extensão está impedindo a conexão com nossos serviços, afetando a sincronização em tempo real.</p>
             <ul class="mt-2 list-disc pl-4 space-y-1">
               <li>Dados não serão atualizados automaticamente</li>
               <li>Você precisará atualizar manualmente a página para ver novos dados</li>
@@ -51,7 +53,7 @@ defineExpose({ dismiss });
             </ol>
           </div>
         </div>
-        <div class="ml-auto pl-3">
+        <div class="ml-2 sm:ml-auto pl-1 sm:pl-3">
           <div class="-mx-1.5 -my-1.5">
             <button 
               @click="dismiss" 
